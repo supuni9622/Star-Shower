409bcc1654a6e8f527076be7a49677cc7c2c1092
Supuni22 <supunimanamperi@gmail.com>
2020-11-14 12:56:19 +0530

    Implemented with gravity functions and velocity.

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -22,6 +22,8 @@ canvas.height = innerHeight
 const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
 
 addEventListener('resize', () => {
+  // First need to declare width and height for our canvas. Here we get Width and height of our browser
+  // Canvas coordinates starts from top- left coner of the browser (0,0)
   canvas.width = innerWidth
   canvas.height = innerHeight
 
@@ -35,6 +37,10 @@ function Star (x, y, radius, color) {
     this.y = y
     this.radius = radius
     this.color = color
+    this.velocity = {
+      x : 0,
+      y: 3
+    }
   }
 
   // Create draw and update functions -- this function determine what the circle/star looks like on the screen
@@ -52,17 +58,27 @@ function Star (x, y, radius, color) {
 
  Star.prototype.update = function() {
     this.draw()
+
+    // Create animating through gravitation and add conditions 
+    // When ball hits bottom of the screen
+    if(this.y + this.radius > canvas.height){
+      this.velocity.y = -this.velocity.y
+    }
+
+    this.y += this.velocity.y
+
   }
 
 //=======================================================//
 
-// Implementation
-let objects
+// Implementation  -- creating multiple stars using Star blue print
+let stars
 function init() {
-  objects = []
+  stars = []
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
+  for (let i = 0; i < 1; i++) {
+    // Creating stars by passing arguments
+     stars.push(new Star(canvas.width/2, 30, 30, 'blue'))
   }
 }
 
@@ -71,11 +87,13 @@ function animate() {
   requestAnimationFrame(animate)
   c.clearRect(0, 0, canvas.width, canvas.height)
 
+  stars.forEach(star => {
+    star.update()
+   })
+
   // ******Mouse moving text
   //c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+  
 }
 
 init()
